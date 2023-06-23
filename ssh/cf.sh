af8d3ab1c719b92e0fdb7b7a2a37b35abc8cceac
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2023-06-23 23:11:44 +0700

    Add files via upload

---
+++ ssh/cf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Edition : Stable Edition V3.0
+# Auther  : AndreSakti
+# (C) Copyright 2023
+# =========================================
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+apt install jq curl -y >/dev/null 2>&1
+DOMAIN=andresakti.xyz
+sub=$(</dev/urandom tr -dc a-x1-9 | head -c5 | tr -d '\r' | tr -d '\r\n')
+SUB_DOMAIN=${sub}.andresakti.xyz
+CF_ID=sakti7asa@gmail.com
+CF_KEY=d98bb9cf92a8dcb0b4dbac03afc6a29d5bad4
+set -euo pipefail
+IP=$(wget -qO- ifconfig.me/ip);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo "IP=" >> /var/lib/andresakti7/ipvps.conf
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /root/domain
+rm -f /root/cf.sh
