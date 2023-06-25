ee4c9d844d6f6e86e6759e644522875cae85de75
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2023-06-26 03:38:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -868,7 +868,8 @@ function menu(){
     print_install "Memasang Menu Packet"
     wget -O ~/menu.zip "${REPO}menu/menu.zip" >/dev/null 2>&1
     mkdir /root/menu
-    7z e -pmeki   ~/menu.zip -o/root/menu/ >/dev/null 2>&1
+    unzip menu.zip; -o/root/menu/ >/dev/null 2>&1
+#    7z e -pmeki   ~/menu.zip -o/root/menu/ >/dev/null 2>&1
     chmod +x /root/menu/*
     mv /root/menu/* /usr/local/sbin/
     #botmintod
