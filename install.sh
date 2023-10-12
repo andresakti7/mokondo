f139a31198852d83f8aa1e4b9d3bb4833123177d
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2023-10-12 23:33:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -315,8 +315,8 @@ function base_package() {
     apt update -y
     sudo apt-get install python3.7-dev 
     apt install python3.7-distutils
-    python3.7 -m pip install telethon
-    pip3 install telethon==1.29.3
+#    python3.7 -m pip install telethon
+    python3.7 -m pip install telethon==1.29.3
     python3.7 -m pip install pyarmor
     print_success "Packet Yang Dibutuhkan"
     
