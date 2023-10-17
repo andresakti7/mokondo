fad5ac0b25437dd726afb52b85de84b5d6b1fa4c
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2023-10-18 01:47:22 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ echo -e " \e[1;97;101m          SCRIPT HAS BEEN UPDATED        \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m update script service\033[1;37m"
-fun_bar 'res1'
+#fun_bar 'res1'
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -n 1 -s -r -p "Press [ Enter ] to back on menu"
